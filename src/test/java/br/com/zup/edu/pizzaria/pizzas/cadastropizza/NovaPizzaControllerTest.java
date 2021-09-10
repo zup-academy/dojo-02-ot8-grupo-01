c175cf2c922bf2cf88f5a4b97622a53b4278df27
AlanaZUP <alana.reis@zup.com.br>
2021-09-10 17:31:26 -0300

    piloto Alana co-piloto Everton

--- src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
@@ -2,14 +2,13 @@ package br.com.zup.edu.pizzaria.pizzas.cadastropizza;
 
 import br.com.zup.edu.pizzaria.ingredientes.Ingrediente;
 import br.com.zup.edu.pizzaria.ingredientes.IngredienteRepository;
-import br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes.NovoIngredienteRequest;
-import br.com.zup.edu.pizzaria.pizzas.Pizza;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import javax.transaction.Transactional;
@@ -27,6 +26,9 @@ public class NovaPizzaControllerTest {
 
 
 
+    @Autowired
+    private MockMvc mvc;
+
     Ingrediente ingrediente1 = new Ingrediente("farinha", 10, new BigDecimal(10.50) );
     Ingrediente ingrediente2 = new Ingrediente("molho", 5, new BigDecimal(1.50) );
     Ingrediente ingrediente3 = new Ingrediente("oregano", 5, new BigDecimal(3.50) );
@@ -36,7 +38,7 @@ public class NovaPizzaControllerTest {
 
 
     @Test
-    public void cadastrarPizzaComSucesso(){
+    public void cadastrarPizzaComSucesso() throws Exception {
 
             Long id1 =  ingredienteRepository.save(ingrediente1).getId();
             Long id2 =  ingredienteRepository.save(ingrediente2).getId();
@@ -53,10 +55,43 @@ public class NovaPizzaControllerTest {
         mvc.perform(request)
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"))
-                .andExpect(redirectedUrlPattern("/api/ingredientes/{id}"));
-
-
-
+                .andExpect(redirectedUrlPattern("api/pizzas/{id}"));
     }
 
+    @Test
+    public void deveRecusarPizzaComSaborRepetido() throws Exception {
+
+        Long id1 =  ingredienteRepository.save(ingrediente1).getId();
+        Long id2 =  ingredienteRepository.save(ingrediente2).getId();
+        Long id3 =  ingredienteRepository.save(ingrediente3).getId();
+
+        List<Long> list = List.of(id1, id2, id3);
+
+        NovaPizzaRequest pizzaRequest = new NovaPizzaRequest("Sem gosto", list);
+
+        MockHttpServletRequestBuilder request = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(pizzaRequest));
+
+        MockHttpServletRequestBuilder request2 = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(pizzaRequest));
+
+        mvc.perform(request);
+        mvc.perform(request2)
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deveRecusarPizzaSemIngredientes() throws Exception {
+        List<Long> list = new ArrayList<>();
+        NovaPizzaRequest pizzaRequest = new NovaPizzaRequest("Ruim", list);
+
+        MockHttpServletRequestBuilder request = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(pizzaRequest));
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
 }
